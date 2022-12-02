aafb21107d366c02f084e02d314f4c78067a0d58
murqa <krivosheevnikolay1@yandex.ru>
2022-12-02 22:33:32 +0500

    добавлен компонент продукта

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { IProduct } from './models/product';
+import { products as data } from './data/products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'price-list-angular';
+  title = 'angular app';
+
+  products: IProduct[] = data;
 }
